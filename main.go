f52aea88f6241fb2464049c79e3db2afaa8f124f
MansoorCM <manzoorcm96@gmail.com>
2025-03-16 11:28:44 +0530

    created a basic web server

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("welcome")
+
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Starting the sever at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("couldn't start http server")
+	}
+
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world !")
 }
