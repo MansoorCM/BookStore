7c6c752077ec52c09f1cf36d4c1dc2373988d661
MansoorCM <manzoorcm96@gmail.com>
2025-04-14 13:09:52 +0530

    Added gorilla/mux to handle more complex routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,21 +6,24 @@ import (
 	"net/http"
 
 	"github.com/MansoorCM/gobook/internal/api"
+	"github.com/MansoorCM/gobook/internal/middleware"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("welcome")
 
-	mux := http.NewServeMux()
+	r := mux.NewRouter()
+	r.Use(middleware.LoggingMiddleWare)
 
 	handler := &api.MessageHandler{Message: "welcome to the bookstore"}
 
-	mux.Handle("/", handler)
-	mux.HandleFunc("/books", api.BooksHandler)
-	mux.HandleFunc("/order", api.OrderHandler)
+	r.Handle("/", handler)
+	r.HandleFunc("/books", api.BooksHandler).Methods("GET")
+	r.HandleFunc("/order", api.OrderHandler).Methods("GET")
 
 	fmt.Println("Starting the sever at port 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("couldn't start http server")
 	}
 
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/MansoorCM/gobook
 go 1.23.0
 
 toolchain go1.23.7
+
+require github.com/gorilla/mux v1.8.1 // indirect
