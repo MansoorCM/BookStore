96f18d454636f781fb1c184f8b512fc1f69f19eb
MansoorCM <manzoorcm96@gmail.com>
2025-04-16 12:54:03 +0530

    Added function to get book by id

--- internal/model/book.go
+++ internal/model/book.go
@@ -1,7 +1,23 @@
 package model
 
+import "errors"
+
 type Book struct {
 	ID     string
 	Title  string
 	Author string
+}
+
+var books = []*Book{
+	{ID: "1", Title: "Think and Grow Rich", Author: "Nepolean Hill"},
+	{ID: "2", Title: "The power of positive thinking", Author: "Norman Vincent Peale"},
+}
+
+func getBookByID(id string) (*Book, error) {
+	for _, book := range books {
+		if book.ID == id {
+			return book, nil
+		}
+	}
+	return nil, errors.New("Book not found")
 }
